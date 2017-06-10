5a1bc6f329b6a32e182ee45e6502b37d08876246
Dmitrii Sokolov <sokolovdp@gmail.com>
2017-06-11 00:11:32 +0200

    1st version

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 # Github Trends
 
-[TODO. There will be project description]
+usage:
+------
+```
+python github_trending.py [--help] --n N   --date DATE
 
+this program prints N top GitHub repositories from the given date
+
+arguments:
+  --help       show this help message and exit
+  --n N        number of repositories
+  --date DATE  date, in the format: YYYY-MM-DD
+```
+usage:
+------
+```
+python github_trending.py  --n=20  --date=2017-06-04
+```
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,29 @@
+import requests
+import sys
+import argparse
+from pprint import pprint
 
 
-def get_trending_repositories(top_size):
-    pass
+def get_response(url, par):
+    response = requests.get(url, par)
+    if response.ok:
+        return response.json()['items']
+    else:
+        print("github api error:")
+        pprint(response)
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def main(n: "int", date: "str"):
+    parameters = {'q': 'created:>{}'.format(date), 'sort': 'stars', 'order': 'desc'}
+    trending_repositories = get_response('https://api.github.com/search/repositories', parameters)[:n]
+    for rep in trending_repositories:
+        print("{} {}, open issues: {}".format(rep['html_url'], rep['description'].strip(), rep['open_issues_count']))
+
 
 if __name__ == '__main__':
-    pass
+    ap = argparse.ArgumentParser(description='this program prints N top GitHub repositories from the given date')
+    ap.add_argument("--n", dest="n", action="store", type=int, required=True, help="  number of repositories")
+    ap.add_argument("--date", dest="date", action="store", required=True, help="  date, in the format: YYYY-MM-DD")
+    args = ap.parse_args(sys.argv[1:])
+
+    main(args.n, args.date)
